1c3a77cddd27a252bd38dd4be5bafb6f82751d4c
Nguyen Minh Tuan <tnowad@gmail.com>
2022-12-22 05:48:16 +0700

    add class singleton

---
+++ singleton/Singleton.ts
@@ -0,0 +1,10 @@
+export class Singleton {
+  private static instance: Singleton;
+  private constructor() {}
+  public static getInstance(): Singleton {
+    if (!Singleton.instance) {
+      Singleton.instance = new Singleton();
+    }
+    return this.instance;
+  }
+}
