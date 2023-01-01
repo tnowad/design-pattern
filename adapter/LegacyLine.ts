422bb9c28b9db6d34afaf0c3ac3d2fa8abf240b4
Nguyen Minh Tuan <tnowad@gmail.com>
2023-01-02 06:07:41 +0700

    add legacy class

---
+++ adapter/LegacyLine.ts
@@ -0,0 +1,5 @@
+export class LegacyLine {
+  draw(x1: number, y1: number, x2: number, y2: number) {
+    console.log(`Drawing line (${x1}, ${x2}), (${x2}, ${y2})`);
+  }
+}
---
+++ adapter/LegacyRectangle.ts
@@ -0,0 +1,7 @@
+export class LegacyRectangle {
+  draw(x: number, y: number, width: number, height: number) {
+    console.log(
+      `Drawing line (${x}, ${y}), width: ${width}, height: ${height})`
+    );
+  }
+}
